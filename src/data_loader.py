70e62768b51ff71e3b803fa455886e696158c760
Carlos Vergara Del Rio <carlosvergarachile@gmail.com>
2026-09-15 17:03:29 -0300

    Delete src/data_loader.py

--- src/data_loader.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import yaml
-def load_settings():
-    try:
-        with open("config/settings.yaml", "r") as f: return yaml.safe_load(f)
-    except: return {"volatility_thresholds": {"max_return": 0.5, "min_return": -0.5}}
-def load_trades(): return pd.read_csv("data/trades.csv")
-def load_algorithms(): return pd.read_csv("data/algorithms.csv")
